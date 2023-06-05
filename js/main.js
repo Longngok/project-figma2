0dda7fa8ea87047f54f9d430e512628335376f46
Longngok <bboylongngok193@gmail.com>
2023-06-05 19:50:55 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    menu.style.display = "flex";
+}
+
+function hideMenu() {
+    var menu = document.getElementById("menu");
+    menu.style.display = "none";
+}
