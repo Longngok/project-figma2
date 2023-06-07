bda1e043f18b83e374ba6d14df7a945b1d6cf697
Longngok <bboylongngok193@gmail.com>
2023-06-07 20:21:51 +0700

    first commit

--- js/main.js
+++ js/main.js
@@ -7,3 +7,26 @@ function hideMenu() {
     var menu = document.getElementById("menu");
     menu.style.display = "none";
 }
+
+file: script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    var playButton = document.getElementById('play-button');
+    var videoIframe = document.getElementById('video-iframe');
+  
+    playButton.addEventListener('click', function() {
+      if (videoIframe.paused) {
+        videoIframe.play();
+        playButton.style.display = 'none';
+      } else {
+        videoIframe.pause();
+        playButton.style.display = 'block';
+      }
+    });
+  
+    videoIframe.addEventListener('ended', function() {
+      playButton.style.display = 'block';
+    });
+  });
+  
+  
